ccef2fcc29797828bbc0126f58977dbcf75119d4
micchyboy1023 <masamichi1023@gmail.com>
2017-03-03 00:27:32 +0900

    Initial commit

---
+++ CameraTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraTest
+//
+//  Created by Masamichi Ueta on 2017/03/03.
+//  Copyright © 2017 Masamichi Ueta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
